cbf3f01d53c451d87e98588f56858e9970966920
after <766256898@qq.com>
2020-08-20 21:59:42 +0800

    优化树形结构

--- src/main/java/com/maidi/saas/biz/impl/TaskManagerImpl.java
+++ src/main/java/com/maidi/saas/biz/impl/TaskManagerImpl.java
@@ -26,17 +26,17 @@ import java.util.Map;
 public class TaskManagerImpl implements TaskManager {
 
     @Autowired
-    private CommonTaskDao taskDao;
+    private CommonTaskDao commonTaskDao;
 
     @Override
     public Map tree() {
         Map result = new HashMap();
-        List<TreeVo> projects = taskDao.getProjects(0);
+        List<TreeVo> projects = commonTaskDao.getProjects(0);
         List<TreeVo> subTasks;
         SearchDict dict = new SearchDict();
         for (TreeVo treeVo : projects) {
             dict.setProjectId(treeVo.getProjectId());
-            subTasks = taskDao.listTree(dict);
+            subTasks = commonTaskDao.listTree(dict);
             if (subTasks.size() > 0) {
                 treeVo.setSubTree(subTasks);
                 treeVo.setParent(true);
@@ -50,33 +50,30 @@ public class TaskManagerImpl implements TaskManager {
     @Override
     public Map subtree(SearchDict dict) {
         Map result = new HashMap();
-        List<TreeVo> subTasks = new ArrayList<>();
+        List<TreeVo> subTask = new ArrayList<>();
         String str = dict.getId();
         int id = IdUtil.paresId(str);
         dict.setId(String.valueOf(id));
         if (dict.getFlag() == 0) {
             dict.setProjectId(id);
-            subTasks = taskDao.listTree(dict);
-            buildTree(dict, subTasks, dict.getFlag());
-            log.warn("dict {} size {}", dict, subTasks.size());
+            subTask = commonTaskDao.listTree(dict);
+            buildTree(dict, subTask, dict.getFlag());
         } else if (dict.getFlag() == 1) {
             dict.setParentId(id);
-            TreeVo treeVo = taskDao.getTreeById(dict);
+            TreeVo treeVo = commonTaskDao.getTreeById(dict);
             dict.setLevel(treeVo.getLevel() + 1);
-            subTasks = taskDao.listTree(dict);
-            if (subTasks.size() == 0) {
-                dict.setFlag(2);
-                dict.setParentId(0);
-                dict.setTaskId(Integer.parseInt(treeVo.getId()));
-                subTasks = taskDao.listProduct(dict);
+            subTask = commonTaskDao.listTree(dict);
+            if (subTask.size() == 0) {
+                subTask = skipLevel(subTask, treeVo, dict);
             }
-            buildTree(dict, subTasks, dict.getFlag());
+            buildTree(dict, subTask, dict.getFlag());
+
         } else if (dict.getFlag() == 2) {
             dict.setParentId(id);
-            subTasks = taskDao.listProduct(dict);
-            buildTree(dict, subTasks, dict.getFlag());
+            subTask = commonTaskDao.listProduct(dict);
+            buildTree(dict, subTask, dict.getFlag());
         }
-        result.put("data", subTasks);
+        result.put("data", subTask);
         return result;
 
     }
@@ -87,46 +84,41 @@ public class TaskManagerImpl implements TaskManager {
             level = vo.getLevel() + 1;
             if (flag == 0) {
                 dict.setProjectId(vo.getProjectId());
-            } else if (flag==1){
+            } else if (flag == 1) {
                 dict.setParentId(Integer.parseInt(vo.getId()));
                 dict.setProjectId(0);
                 dict.setTaskId(0);
-            }else {
+            } else {
                 dict.setParentId(Integer.parseInt(vo.getId()));
                 dict.setProjectId(0);
                 dict.setTaskId(0);
             }
             dict.setLevel(level);
-            subTask(dict, vo, flag);
+            nextLevel(dict, vo, flag);
         }
     }
 
-    private void subTask(SearchDict dict, TreeVo treeVo, int flag) {
+    private void nextLevel(SearchDict dict, TreeVo treeVo, int flag) {
         int level = dict.getLevel();
         List<TreeVo> subTask;
         if (flag == 1) {
-            subTask = taskDao.listTree(dict);
+            subTask = commonTaskDao.listTree(dict);
             if (subTask.size() == 0) {
-                dict.setFlag(2);
-                dict.setParentId(0);
-                dict.setTaskId(Integer.parseInt(treeVo.getId()));
-                subTask = taskDao.listProduct(dict);
+                subTask = skipLevel(subTask, treeVo, dict);
             }
             buildTree(dict, subTask, dict.getFlag());
-            log.warn("dict {} size {}",dict,subTask.size());
         } else if (flag == 2) {
-            subTask = taskDao.listProduct(dict);
+            subTask = commonTaskDao.listProduct(dict);
         } else {
             dict.setParentId(Integer.parseInt(treeVo.getId()));
             dict.setProjectId(0);
             dict.setTaskId(0);
-            subTask = taskDao.listTree(dict);
-            log.warn("subDict {}", dict);
+            subTask = commonTaskDao.listTree(dict);
             if (subTask.size() == 0) {
                 flag = 2;
                 dict.setParentId(0);
                 dict.setTaskId(Integer.parseInt(treeVo.getId()));
-                subTask = taskDao.listProduct(dict);
+                subTask = commonTaskDao.listProduct(dict);
             }
         }
         treeVo.setSubTree(subTask);
@@ -137,4 +129,14 @@ public class TaskManagerImpl implements TaskManager {
         }
     }
 
+    public List<TreeVo> skipLevel(List<TreeVo> subTask, TreeVo treeVo, SearchDict dict) {
+        if (subTask.size() == 0) {
+            dict.setFlag(2);
+            dict.setParentId(0);
+            dict.setTaskId(Integer.parseInt(treeVo.getId()));
+            treeVo.setParent(false);
+            subTask = commonTaskDao.listProduct(dict);
+        }
+        return subTask;
+    }
 }
